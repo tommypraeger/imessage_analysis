1e6fc47dc3e53650998522460c02430dc23fa261
Tommy Praeger <thomaspraeger2020@u.northwestern.edu>
2020-03-27 16:40:22 -0500

    Create example user_data.py

---
+++ user_data.py
@@ -0,0 +1,17 @@
+USERNAME = 'yourusername'
+CHAT_IDS = {
+    'group 1': 367,
+    'group 2': 16,
+    'friend 1': 111,
+    'friend 2': 104
+}
+CONTACT_ID_TO_NAME = {
+    0: 'your name',
+    117: 'friend 1',
+    112: 'friend 2'
+}
+CONTACT_NAME_TO_ID = {
+    'your name': 0,
+    'friend 1': 117,
+    'friend 2': 112
+}
