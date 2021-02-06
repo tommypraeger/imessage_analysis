a8a5341801d986392927557781343dfc118f0e0e
Tommy Praeger <tommypraeger@gmail.com>
2021-02-06 10:45:32 -0700

    forgot to add this file when changing emojis to emoji

--- analysis/functions/emoji.py
+++ analysis/functions/emoji.py
@@ -4,15 +4,15 @@ import analysis.utils.helpers as helpers
 
 
 def main(result_dict, df, chat_members):
-    result_dict['messages that contain emojis'] = []
-    result_dict['% of messages that include emojis'] = []
+    result_dict['messages that contain emoji'] = []
+    result_dict['% of messages that include emoji'] = []
     df['includes emoji?'] = df['text'].apply(helpers.includes_emoji)
     for member_name in chat_members:
         _, non_reaction_messages = initialize_result_dict(member_name, df, result_dict)
         emoji_messages = len(
             df[(df['includes emoji?']) & (df['sender'] == member_name)]
         )
-        result_dict['messages that contain emojis'].append(emoji_messages)
-        result_dict['% of messages that include emojis'].append(
+        result_dict['messages that contain emoji'].append(emoji_messages)
+        result_dict['% of messages that include emoji'].append(
             round(helpers.safe_divide(emoji_messages, non_reaction_messages) * 100, 2)
         )
