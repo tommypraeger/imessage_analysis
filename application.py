27f3b6ed609f9fd7cdb6dbdf74fe1137322f6110
Tommy Praeger <tommypraeger@gmail.com>
2021-08-08 16:46:18 -0700

    refactoring application.py

--- application.py
+++ application.py
@@ -16,30 +16,8 @@ CORS(application)
 
 class Application(Resource):
     def get(self, action):
-        try:
-            args = ['python3', '-m', 'analysis', action]
-            output = self.get_output(args)
-        except subprocess.CalledProcessError as e:
-            # Get exception from stack trace
-            error = str(e.output).split('\\n')[-2][11:]
-            print(error)
-            return {
-                'errorMessage': error['message']
-            }, 400
-
-        try:
-            output = json.loads(output)
-        except json.decoder.JSONDecodeError:
-            print(output)
-            return {
-                'errorMessage': output
-            }, 400
-
-        if 'errorMessage' in output:
-            print(output['errorMessage'])
-            return output, 400
-
-        return output, 200
+        args = ['python3', '-m', 'analysis', action]
+        return self.get_output(args)
 
     def post(self, action):
         parser = reqparse.RequestParser()
@@ -53,25 +31,26 @@ class Application(Resource):
             if val != '':
                 args_list.append(val)
 
+        args = ['python3', '-m', 'analysis', action]
+        args.extend(args_list)
+        return self.get_output(args)
+
+    def get_output(self, args):
         try:
-            args = ['python3', '-m', 'analysis', action]
-            args.extend(args_list)
-            output = self.get_output(args)
+            args = self.convert_args_to_strs(args)
+            output = subprocess.run(args, stdout=subprocess.PIPE)
+            output = output.stdout.decode('utf-8')
         except subprocess.CalledProcessError as e:
             # Get exception from stack trace
             error = str(e.output).split('\\n')[-2][11:]
             print(error)
-            return {
-                'errorMessage': error
-            }, 400
+            return self.error_msg(error['message'], 400)
 
         try:
             output = json.loads(output)
         except json.decoder.JSONDecodeError:
             print(output)
-            return {
-                'errorMessage': output
-            }, 400
+            return self.error_msg(output, 400)
 
         if 'errorMessage' in output:
             print(output['errorMessage'])
@@ -79,13 +58,13 @@ class Application(Resource):
 
         return output, 200
 
-    def get_output(self, args):
-        args = self.convert_args_to_strs(args)
-        output = subprocess.run(args, stdout=subprocess.PIPE)
-        return output.stdout.decode('utf-8')
-
     def convert_args_to_strs(self, args):
         return [str(arg) for arg in args]
+
+    def error_msg(self, msg, code):
+        return {
+            'errorMessage': msg
+        }, code
 
 
 api.add_resource(Application, '/api/v1/<string:action>')
