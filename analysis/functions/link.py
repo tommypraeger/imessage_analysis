dd4385ae99f8557d2c54cd196ef150dba9c7813f
Tommy Praeger <tommypraeger@gmail.com>
2021-08-24 22:48:19 -0700

    incomplete - making link, messages series, and mime type graphable

--- analysis/functions/link.py
+++ analysis/functions/link.py
@@ -1,18 +1,57 @@
-import analysis.utils.constants as constants
+from analysis.utils.constants import GRAPH_TOTAL_KEY
 import analysis.utils.helpers as helpers
 
+links_column = 'Messages that are links'
+percent_links_column = 'Percent of messages that are links'
 
-def main(result_dict, df, chat_members):
-    result_dict['messages that are links'] = []
-    result_dict['% of messages that are links'] = []
+
+def get_columns():
+    return [
+        links_column,
+        percent_links_column
+    ]
+
+
+def get_columns_allowing_graph_total():
+    return [
+        links_column,
+        percent_links_column
+    ]
+
+
+def process_df(df):
     df['is link?'] = df['text'].apply(helpers.is_link)
+
+
+def get_results(output_dict, df, member_name=None, time_period=None):
+    nr_messages = helpers.get_non_reaction_messages(df, member_name, time_period)
+    link_messages = len(nr_messages[nr_messages['is link?']])
+    output_dict[links_column].append(link_messages)
+    output_dict[percent_links_column].append(
+        round(helpers.safe_divide(link_messages, len(nr_messages)) * 100, 2))
+
+
+def get_table_results(result_dict, df, chat_members, args):
+    process_df(df)
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        non_reaction_messages = helpers.get_total_non_reaction_messages(df, member_name)
-        link_messages = len(
-            df[(df['is link?']) & (df['sender'] == member_name)]
-        )
-        result_dict['messages that are links'].append(link_messages)
-        result_dict['% of messages that are links'].append(
-            round(helpers.safe_divide(link_messages, non_reaction_messages) * 100, 2)
-        )
+        get_results(result_dict, df, member_name)
+
+
+def get_graph_results(graph_data, df, chat_members, time_periods, args):
+    process_df(df)
+    if args.graph_individual:
+        get_individual_graph_results(graph_data, df, chat_members, time_periods)
+    else:
+        get_total_graph_results(graph_data, df, time_periods)
+
+
+def get_individual_graph_results(graph_data, df, chat_members, time_periods):
+    for time_period in time_periods:
+        for member_name in chat_members:
+            get_results(graph_data[member_name], df, member_name, time_period)
+
+
+def get_total_graph_results(graph_data, df, time_periods):
+    for time_period in time_periods:
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, None, time_period)
--- analysis/functions/mime_type.py
+++ analysis/functions/mime_type.py
@@ -1,21 +1,61 @@
-import analysis.utils.constants as constants
+from analysis.utils.constants import GRAPH_TOTAL_KEY
 import analysis.utils.helpers as helpers
 
+type_column = 'Messages that are the selected file type'
+percent_type_column = 'Percent of messages that are the selected file type'
 
-def main(result_dict, df, chat_members, args):
-    mime_type = args.mime_type
+
+def get_columns():
+    return [
+        type_column,
+        percent_type_column
+    ]
+
+
+def get_columns_allowing_graph_total():
+    return [
+        type_column,
+        percent_type_column
+    ]
+
+
+def process_df(df, mime_type):
     if mime_type is None:
         raise Exception('Function is type but not given a type')
-    result_dict[f'{mime_type} messages'] = []
-    result_dict[f'% of messages that are file type {mime_type}'] = []
     df[f'is file type {mime_type}?'] = df['type'].apply(lambda typ: helpers.is_type(typ, mime_type))
+
+
+def get_results(output_dict, df, mime_type, member_name=None, time_period=None):
+    nr_messages = helpers.get_non_reaction_messages(df, member_name, time_period)
+    mime_type_messages = len(nr_messages[nr_messages[f'is file type {mime_type}?']])
+    output_dict[type_column].append(mime_type_messages)
+    output_dict[percent_type_column].append(
+        round(helpers.safe_divide(mime_type_messages, len(nr_messages)) * 100, 2))
+
+
+def get_table_results(result_dict, df, chat_members, args):
+    mime_type = args.mime_type
+    process_df(df, mime_type)
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        non_reaction_messages = helpers.get_total_non_reaction_messages(df, member_name)
-        mime_type_messages = len(
-            df[(df[f'is file type {mime_type}?']) & (df['sender'] == member_name)]
-        )
-        result_dict[f'{mime_type} messages'].append(mime_type_messages)
-        result_dict[f'% of messages that are file type {mime_type}'].append(
-            round(helpers.safe_divide(mime_type_messages, non_reaction_messages) * 100, 2)
-        )
+        get_results(result_dict, df, mime_type, member_name)
+
+
+def get_graph_results(graph_data, df, chat_members, time_periods, args):
+    mime_type = args.mime_type
+    process_df(df, mime_type)
+    if args.graph_individual:
+        get_individual_graph_results(graph_data, df, mime_type, chat_members, time_periods)
+    else:
+        get_total_graph_results(graph_data, df, mime_type, time_periods)
+
+
+def get_individual_graph_results(graph_data, df, mime_type, chat_members, time_periods):
+    for time_period in time_periods:
+        for member_name in chat_members:
+            get_results(graph_data[member_name], df, mime_type, member_name, time_period)
+
+
+def get_total_graph_results(graph_data, df, mime_type, time_periods):
+    for time_period in time_periods:
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, mime_type, None, time_period)
