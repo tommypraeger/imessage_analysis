2dfb0f2c5389be908c3c4cf247776692943c0ca8
Tommy Praeger <tommypraeger@gmail.com>
2021-08-24 22:48:19 -0700

    incomplete - making link, messages series, and mime type graphable

--- analysis/functions/message_series.py
+++ analysis/functions/message_series.py
@@ -1,13 +1,36 @@
-import analysis.utils.constants as constants
+from analysis.functions import total
+from analysis.utils.constants import GRAPH_TOTAL_KEY
 import analysis.utils.helpers as helpers
 
 
 def main(result_dict, df, chat_members, args):
-    result_dict['total # of message series'] = []
-    result_dict['total messages'] = []
-    result_dict['average messages per series'] = []
+    for member_name in chat_members:
+        helpers.initialize_member(member_name, result_dict)
+
+
+message_series_column = 'Total number of message series'
+average_messages_column = 'Average messages per series'
+percent_series_column = 'Percent of total message series'
+
+
+def get_columns():
+    return [
+        message_series_column,
+        average_messages_column,
+        percent_series_column
+    ]
+
+
+def get_columns_allowing_graph_total():
+    return [
+        message_series_column,
+        average_messages_column
+    ]
+
+
+def process_df(df, minutes_threshold):
     df['is conversation starter?'] = df['time'].diff().apply(
-        lambda diff: helpers.is_conversation_starter(diff, args.minutes_threshold)
+        lambda diff: helpers.is_conversation_starter(diff, minutes_threshold)
     )
     df.iloc[0, df.columns.get_loc('is conversation starter?')] = True
     df['is new message series?'] = df['sender'].apply(lambda x: True)
@@ -15,16 +38,51 @@ def main(result_dict, df, chat_members, args):
         df['sender'].shift() != df['sender'], False
     )
     df.iloc[0, df.columns.get_loc('is new message series?')] = True
+
+
+def get_results(output_dict, df, member_name=None, time_period=None):
+    messages = helpers.get_messages(df, member_name, time_period)
+    message_series = len(messages[(messages['is new message series?'])
+                         | (messages['is conversation starter?'])])
+    output_dict[message_series_column].append(message_series)
+    output_dict[average_messages_column].append(
+        round(helpers.safe_divide(len(messages), message_series), 2)
+    )
+    return message_series
+
+
+def get_table_results(result_dict, df, chat_members, args):
+    process_df(df, args.minutes_threshold)
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        total_messages = helpers.get_total_messages(df, member_name)
-        message_series = len(
-            df[((df['is new message series?'])
-                | (df['is conversation starter?']))
-                & (df['sender'] == member_name)]
-        )
-        result_dict['total # of message series'].append(message_series)
-        result_dict['total messages'].append(total_messages)
-        result_dict['average messages per series'].append(
-            round(helpers.safe_divide(total_messages, message_series), 2)
-        )
+        get_results(result_dict, df, member_name)
+    total_message_series = sum(result_dict[message_series_column])
+    for i in range(len(result_dict[message_series_column])):
+        result_dict[percent_series_column].append(
+            round(helpers.safe_divide(result_dict[message_series_column]
+                                      [i], total_message_series) * 100, 2))
+
+
+def get_graph_results(graph_data, df, chat_members, time_periods, args):
+    process_df(df, args.minutes_threshold)
+    if args.graph_individual:
+        get_individual_graph_results(graph_data, df, chat_members, time_periods)
+    else:
+        get_total_graph_results(graph_data, df, time_periods)
+
+
+def get_individual_graph_results(graph_data, df, chat_members, time_periods):
+    for time_period in time_periods:
+        total_message_series_in_period = 0
+        for member_name in chat_members:
+            total_message_series_in_period += get_results(
+                graph_data[member_name], df, member_name, time_period)
+        for member_name in chat_members:
+            graph_data[member_name][percent_series_column].append(
+                round(helpers.safe_divide(graph_data[member_name][message_series_column][-1], total_message_series_in_period)
+                      * 100, 2))
+
+
+def get_total_graph_results(graph_data, df, time_periods):
+    for time_period in time_periods:
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, None, time_period)
