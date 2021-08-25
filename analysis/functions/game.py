ef29280bff757e1334d3a80dbbbfd9f96f1a51a4
Tommy Praeger <tommypraeger@gmail.com>
2021-08-24 21:56:33 -0700

    incomplete - making emoji and game graphable

--- analysis/functions/game.py
+++ analysis/functions/game.py
@@ -1,36 +1,76 @@
-import analysis.utils.constants as constants
+from analysis.utils.constants import GRAPH_TOTAL_KEY
 import analysis.utils.helpers as helpers
 
+games_column = 'Messages that are games'
+percent_games_column = 'Percent of messages that are games'
+game_starts_column = 'Messages that start games'
+percent_game_starts_column = 'Percent of games started'
 
-def main(result_dict, df, chat_members):
-    result_dict['messages that are games'] = []
-    result_dict['% of messages that are games'] = []
-    result_dict['messages that are game starts'] = []
-    result_dict['% of game starts that are by this person'] = []
+
+def get_columns():
+    return [
+        games_column,
+        percent_games_column,
+        game_starts_column,
+        percent_game_starts_column
+    ]
+
+
+def get_columns_allowing_graph_total():
+    return [
+        games_column,
+        percent_games_column,
+        game_starts_column
+    ]
+
+
+def process_df(df):
     df['is game message?'] = df.apply(
         lambda msg: helpers.is_game_message(msg.text, msg.type), axis=1)
     df['is game start?'] = df.apply(lambda msg: helpers.is_game_start(msg.text, msg.type), axis=1)
+
+
+def get_results(output_dict, df, member_name=None, time_period=None):
+    nr_messages = helpers.get_non_reaction_messages(df, member_name, time_period)
+    game_messages = len(nr_messages[nr_messages['is game message?']])
+    game_starts = len(nr_messages[nr_messages['is game start?']])
+    output_dict[games_column].append(game_messages)
+    output_dict[percent_games_column].append(
+        round(helpers.safe_divide(game_messages, nr_messages) * 100, 2))
+    output_dict[game_starts_column].append(game_starts)
+    return game_starts
+
+
+def get_table_results(result_dict, df, chat_members, args):
+    process_df(df)
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        non_reaction_messages = helpers.get_total_non_reaction_messages(df, member_name)
-        game_messages = len(
-            df[(df['is game message?']) & (df['sender'] == member_name)]
-        )
-        game_starts = len(
-            df[(df['is game start?']) & (df['sender'] == member_name)]
-        )
-        result_dict['messages that are games'].append(game_messages)
-        result_dict['% of messages that are games'].append(
-            round(helpers.safe_divide(game_messages, non_reaction_messages) * 100, 2)
-        )
-        result_dict['messages that are game starts'].append(game_starts)
-    total_game_starts = sum(result_dict['messages that are game starts'])
-    for i in range(len(result_dict['messages that are game starts'])):
-        result_dict['% of game starts that are by this person'].append(
-            round(
-                helpers.safe_divide(
-                    result_dict['messages that are game starts'][i], total_game_starts
-                ) * 100,
-                2
-            )
-        )
+        get_results(result_dict, df, member_name)
+    total_game_starts = sum(result_dict[game_starts_column])
+    for i in range(len(result_dict[game_starts_column])):
+        result_dict[percent_game_starts_column].append(round(
+            helpers.safe_divide(result_dict[game_starts_column][i], total_game_starts) * 100, 2))
+
+
+def get_graph_results(graph_data, df, chat_members, time_periods, args):
+    process_df(df)
+    if args.graph_individual:
+        get_individual_graph_results(graph_data, df, chat_members, time_periods)
+    else:
+        get_total_graph_results(graph_data, df, time_periods)
+
+
+def get_individual_graph_results(graph_data, df, chat_members, time_periods):
+    for time_period in time_periods:
+        total_games_started_in_period = 0
+        for member_name in chat_members:
+            total_games_started_in_period += get_results(
+                graph_data[member_name], df, member_name, time_period)
+        for member_name in chat_members:
+            graph_data[member_name][percent_game_starts_column].append(round(helpers.safe_divide(
+                graph_data[member_name][game_starts_column][-1], total_games_started_in_period) * 100, 2))
+
+
+def get_total_graph_results(graph_data, df, time_periods):
+    for time_period in time_periods:
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, None, time_period)
--- analysis/functions/total.py
+++ analysis/functions/total.py
@@ -19,14 +19,15 @@ def get_columns_allowing_graph_total():
 
 
 def get_results(output_dict, df, member_name=None, time_period=None):
-    pass
+    total_messages_by_member = helpers.get_total_messages(df, member_name, time_period)
+    output_dict[total_messages_column].append(total_messages_by_member)
+    return total_messages_by_member
 
 
-def get_table_results(result_dict, df, chat_members, args=None):
+def get_table_results(result_dict, df, chat_members, args):
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        total_messages_by_member = helpers.get_total_messages(df, member_name)
-        result_dict[total_messages_column].append(total_messages_by_member)
+        get_results(result_dict, df, member_name)
     total_messages = sum(result_dict[total_messages_column])
     for i in range(len(result_dict[total_messages_column])):
         result_dict[percent_total_messages_column].append(
@@ -37,18 +38,17 @@ def get_table_results(result_dict, df, chat_members, args=None):
 
 def get_graph_results(graph_data, df, chat_members, time_periods, args):
     if args.graph_individual:
-        get_individual_graph_data(graph_data, df, chat_members, time_periods)
+        get_individual_graph_results(graph_data, df, chat_members, time_periods)
     else:
-        get_total_graph_data(graph_data, df, time_periods)
+        get_total_graph_results(graph_data, df, time_periods)
 
 
-def get_individual_graph_data(graph_data, df, chat_members, time_periods):
+def get_individual_graph_results(graph_data, df, chat_members, time_periods):
     for time_period in time_periods:
         total_messages_in_period = 0
         for member_name in chat_members:
-            total_messages_by_member = helpers.get_total_messages(df, member_name, time_period)
-            graph_data[member_name][total_messages_column].append(total_messages_by_member)
-            total_messages_in_period += total_messages_by_member
+            total_messages_in_period += get_results(
+                graph_data[member_name], df, member_name, time_period)
         for member_name in chat_members:
             graph_data[member_name][percent_total_messages_column].append(
                 round(helpers.safe_divide(graph_data[member_name][total_messages_column][-1], total_messages_in_period)
@@ -57,7 +57,6 @@ def get_individual_graph_data(graph_data, df, chat_members, time_periods):
             )
 
 
-def get_total_graph_data(graph_data, df, time_periods):
+def get_total_graph_results(graph_data, df, time_periods):
     for time_period in time_periods:
-        graph_data[GRAPH_TOTAL_KEY][total_messages_column].append(
-            helpers.get_total_messages(df, time_period=time_period))
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, None, time_period)
