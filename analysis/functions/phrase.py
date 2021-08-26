a53b5351f5586395f30a764d8cabcc748c56db5d
Tommy Praeger <tommypraeger@gmail.com>
2021-08-25 21:44:37 -0700

    incomplete - making phrase, tweet, word count, and word length graphable

--- analysis/functions/phrase.py
+++ analysis/functions/phrase.py
@@ -1,22 +1,63 @@
-import analysis.utils.constants as constants
+from analysis.utils.constants import GRAPH_TOTAL_KEY
 import analysis.utils.helpers as helpers
 
+phrase_column = 'Messages that contain the entered phrase'
+percent_phrase_column = 'Percent of messages that contain the entered phrase'
 
-def main(result_dict, df, chat_members, phrase, case_sensitive, separate, regex):
+
+def get_columns():
+    return [
+        phrase_column,
+        percent_phrase_column
+    ]
+
+
+def get_columns_allowing_graph_total():
+    return [
+        phrase_column,
+        percent_phrase_column
+    ]
+
+
+def process_df(df, phrase, case_sensitive, separate, regex):
     if phrase is None:
         raise Exception('Function is phrase but not given a phrase')
-    result_dict[f'messages that contain {phrase}'] = []
-    result_dict[f'% of messages that contain {phrase}'] = []
     df[f'includes {phrase}?'] = df['text'].apply(
         lambda msg: helpers.is_phrase_in(phrase, msg, case_sensitive, separate, regex)
     )
+
+
+def get_results(output_dict, df, phrase, member_name=None, time_period=None):
+    nr_messages = helpers.get_non_reaction_messages(df, member_name, time_period)
+    phrase_messages = len(nr_messages[nr_messages[f'includes {phrase}?']])
+    output_dict[phrase_column].append(phrase_messages)
+    output_dict[percent_phrase_column].append(
+        round(helpers.safe_divide(phrase_messages, len(nr_messages)) * 100, 2))
+
+
+def get_table_results(result_dict, df, chat_members, args):
+    phrase = args.phrase
+    process_df(df, phrase, args.case_sensitive, args.separate, args.regex)
     for member_name in chat_members:
         helpers.initialize_member(member_name, result_dict)
-        non_reaction_messages = helpers.get_total_non_reaction_messages(df, member_name)
-        word_messages = len(
-            df[(df[f'includes {phrase}?']) & (df['sender'] == member_name)]
-        )
-        result_dict[f'messages that contain {phrase}'].append(word_messages)
-        result_dict[f'% of messages that contain {phrase}'].append(
-            round(helpers.safe_divide(word_messages, non_reaction_messages) * 100, 2)
-        )
+        get_results(result_dict, df, phrase, member_name)
+
+
+def get_graph_results(graph_data, df, chat_members, time_periods, args):
+    phrase = args.phrase
+    process_df(df, phrase, args.case_sensitive, args.separate, args.regex)
+    if args.graph_individual:
+        get_individual_graph_results(graph_data, df, phrase, chat_members, time_periods)
+    else:
+        get_total_graph_results(graph_data, df, phrase, time_periods)
+
+
+def get_individual_graph_results(graph_data, df, phrase, chat_members, time_periods):
+    for time_period in time_periods:
+        for member_name in chat_members:
+            get_results(graph_data[member_name], df, phrase, member_name, time_period)
+
+
+def get_total_graph_results(graph_data, df, phrase, time_periods):
+    for time_period in time_periods:
+        get_results(graph_data[GRAPH_TOTAL_KEY], df, phrase, None, time_period)
