a4027c4d19e58221714cd1845f89bafe470cf37b
Tommy Praeger <tommypraeger@gmail.com>
2023-02-16 23:21:34 -0800

    parametrize tests - links, attachments

--- analysis/tests/functions/attachment_test.py
+++ analysis/tests/functions/attachment_test.py
@@ -1,3 +1,4 @@
+import pytest
 from analysis.functions.attachment import (
     Attachment,
     attachment_category,
@@ -6,144 +7,127 @@ from analysis.functions.attachment import (
 from analysis.tests.testutils import *
 
 
-def test_attachment():
-    fn = Attachment()
-    table_actual = [
-        result
-        for result in generate_table_test_result(
-            fn, "attachment", csvs=["group", "non_group"], fn_args_combos=[[]]
-        )
-    ]
-    graph_actual = [
-        result
-        for result in generate_graph_test_result(
-            fn,
-            "attachment",
-            csvs=["group", "non_group"],
-            graph_total_categories=fn.get_categories_allowing_graph_total(),
-            graph_individual_categories=fn.get_categories(),
-            fn_args_combos=[[]],
-        )
-    ]
-    table_expected = [
-        {
-            "description": "group",
-            "names": ["A", "B", "C"],
-            attachment_category: [1, 1, 2],
-            percent_attachment_category: [16.67, 25, 33.33],
-        },
-        {
-            "description": "non-group",
-            "names": ["A", "B"],
-            attachment_category: [2, 2],
-            percent_attachment_category: [22.22, 25],
-        },
-    ]
-    graph_expected = [
-        {
-            "description": "group, total, total messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1, 3],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, total, percent messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [16.67, 30],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, total messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "C",
-                    "data": [1, 1],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, percent messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 25],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 33.33],
-                },
-                {
-                    "label": "C",
-                    "data": [33.33, 33.33],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, total messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1, 3],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, percent messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [16.67, 27.27],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, total messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [1, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 2],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, percent messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [33.33, 16.67],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 40],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    ]
-    assert_table_results_correct(table_actual, table_expected)
-    assert_graph_results_correct(graph_actual, graph_expected)
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            [],
+            {
+                "names": ["A", "B", "C"],
+                attachment_category: [1, 1, 2],
+                percent_attachment_category: [16.67, 25, 33.33],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            {
+                "names": ["A", "B"],
+                attachment_category: [2, 2],
+                percent_attachment_category: [22.22, 25],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(Attachment(), "attachment", csv, fn_args, expected_result)
+
+
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            [],
+            attachment_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1, 3]},
+            },
+        ),
+        (
+            "group",
+            [],
+            percent_attachment_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [16.67, 30]},
+            },
+        ),
+        (
+            "group",
+            [],
+            attachment_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 1], "B": [0, 1], "C": [1, 1]},
+            },
+        ),
+        (
+            "group",
+            [],
+            percent_attachment_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 25], "B": [0, 33.33], "C": [33.33, 33.33]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            attachment_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1, 3]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            percent_attachment_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [16.67, 27.27]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            attachment_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [1, 1], "B": [0, 2]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            percent_attachment_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [33.33, 16.67], "B": [0, 40]},
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        Attachment(),
+        "attachment",
+        csv,
+        fn_args,
+        category,
+        graph_individual,
+        expected_result,
+    )
--- analysis/tests/functions/link_test.py
+++ analysis/tests/functions/link_test.py
@@ -1,145 +1,123 @@
+import pytest
 from analysis.functions.link import Link, links_category, percent_links_category
 from analysis.tests.testutils import *
 
 
-def test_link():
-    fn = Link()
-    table_actual = [
-        result
-        for result in generate_table_test_result(
-            fn, "link", csvs=["group", "non_group"], fn_args_combos=[[]]
-        )
-    ]
-    graph_actual = [
-        result
-        for result in generate_graph_test_result(
-            fn,
-            "link",
-            csvs=["group", "non_group"],
-            graph_total_categories=fn.get_categories_allowing_graph_total(),
-            graph_individual_categories=fn.get_categories(),
-            fn_args_combos=[[]],
-        )
-    ]
-    table_expected = [
-        {
-            "description": "group",
-            "names": ["A", "B", "C"],
-            links_category: [1, 1, 0],
-            percent_links_category: [50.0, 50.0, 0],
-        },
-        {
-            "description": "non-group",
-            "names": ["A", "B"],
-            links_category: [1, 1],
-            percent_links_category: [25.0, 50.0],
-        },
-    ]
-    graph_expected = [
-        {
-            "description": "group, total, total messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1, 1],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, total, percent messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [33.33, 33.33],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, total messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [1, 0],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, percent messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [100.0, 0],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 100.0],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, total messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1, 1],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, percent messages",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [33.33, 33.33],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, total messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [1, 0],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 1],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, percent messsages",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [50.0, 0],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 100.0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    ]
-    assert_table_results_correct(table_actual, table_expected)
-    assert_graph_results_correct(graph_actual, graph_expected)
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            [],
+            {
+                "names": ["A", "B", "C"],
+                links_category: [1, 1, 0],
+                percent_links_category: [50.0, 50.0, 0],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            {
+                "names": ["A", "B"],
+                links_category: [1, 1],
+                percent_links_category: [25.0, 50.0],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(Link(), "link", csv, fn_args, expected_result)
+
+
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            [],
+            links_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1, 1]},
+            },
+        ),
+        (
+            "group",
+            [],
+            percent_links_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [33.33, 33.33]},
+            },
+        ),
+        (
+            "group",
+            [],
+            links_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [1, 0], "B": [0, 1], "C": [0, 0]},
+            },
+        ),
+        (
+            "group",
+            [],
+            percent_links_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [100, 0], "B": [0, 100], "C": [0, 0]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            links_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1, 1]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            percent_links_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [33.33, 33.33]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            links_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [1, 0], "B": [0, 1]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            percent_links_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [50, 0], "B": [0, 100]},
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        Link(), "link", csv, fn_args, category, graph_individual, expected_result
+    )
