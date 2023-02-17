2eaafba1f56ba721a7ead62584ff17fded633f8b
Tommy Praeger <tommypraeger@gmail.com>
2023-02-16 23:05:44 -0800

    parametrize tests - mime type

--- analysis/tests/functions/mime_type_test.py
+++ analysis/tests/functions/mime_type_test.py
@@ -1,3 +1,4 @@
+import pytest
 from analysis.functions.mime_type import (
     MimeType,
     type_category,
@@ -6,269 +7,225 @@ from analysis.functions.mime_type import (
 from analysis.tests.testutils import *
 
 
-def test_mime_type():
-    fn = MimeType()
-    table_actual = [
-        result
-        for result in generate_table_test_result(
-            fn,
-            "mime_type",
-            csvs=["group", "non_group"],
-            fn_args_combos=[
-                ["--mime-type", "text/plain"],
-                ["--mime-type", "image/heic"],
-            ],
-        )
-    ]
-    graph_actual = [
-        result
-        for result in generate_graph_test_result(
-            fn,
-            "mime_type",
-            csvs=["group", "non_group"],
-            graph_total_categories=fn.get_categories_allowing_graph_total(),
-            graph_individual_categories=fn.get_categories(),
-            fn_args_combos=[
-                ["--mime-type", "text/plain"],
-                ["--mime-type", "image/heic"],
-            ],
-        )
-    ]
-    table_expected = [
-        {
-            "description": "group, text/plain",
-            "names": ["A", "B", "C"],
-            type_category: [3, 1, 2],
-            percent_type_category: [50, 25, 33.33],
-        },
-        {
-            "description": "group, image/heic",
-            "names": ["A", "B", "C"],
-            type_category: [1, 0, 0],
-            percent_type_category: [16.67, 0, 0],
-        },
-        {
-            "description": "non-group, text/plain",
-            "names": ["A", "B"],
-            type_category: [3, 4],
-            percent_type_category: [33.33, 50],
-        },
-        {
-            "description": "non-group, image/heic",
-            "names": ["A", "B"],
-            type_category: [1, 0],
-            percent_type_category: [11.11, 0],
-        },
-    ]
-    graph_expected = [
-        {
-            "description": "group, total, total messages, text/plain",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [2, 4],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, total, percent messages, text/plain",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [33.33, 40],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, total messsages, text/plain",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 3],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "C",
-                    "data": [2, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, percent messsages, text/plain",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 75],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 33.33],
-                },
-                {
-                    "label": "C",
-                    "data": [66.67, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, total, total messages, image/heic",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [0, 1],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, total, percent messages, image/heic",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [0, 10],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, total messsages, image/heic",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 0],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, percent messsages, image/heic",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 25],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 0],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, total messages, text/plain",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [2, 5],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, percent messages, text/plain",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [33.33, 45.45],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, total messsages, text/plain",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 3],
-                },
-                {
-                    "label": "B",
-                    "data": [2, 2],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, percent messsages, text/plain",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 50],
-                },
-                {
-                    "label": "B",
-                    "data": [66.67, 40],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, total messages, image/heic",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [0, 1],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, percent messages, image/heic",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [0, 9.09],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, total messsages, image/heic",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, percent messsages, image/heic",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [0, 16.67],
-                },
-                {
-                    "label": "B",
-                    "data": [0, 0],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    ]
-    assert_table_results_correct(table_actual, table_expected)
-    assert_graph_results_correct(graph_actual, graph_expected)
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            ["--mime-type", "text/plain"],
+            {
+                "names": ["A", "B", "C"],
+                type_category: [3, 1, 2],
+                percent_type_category: [50, 25, 33.33],
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "image/heic"],
+            {
+                "names": ["A", "B", "C"],
+                type_category: [1, 0, 0],
+                percent_type_category: [16.67, 0, 0],
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "text/plain"],
+            {
+                "names": ["A", "B"],
+                type_category: [3, 4],
+                percent_type_category: [33.33, 50],
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "image/heic"],
+            {
+                "names": ["A", "B"],
+                type_category: [1, 0],
+                percent_type_category: [11.11, 0],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(MimeType(), "mime_type", csv, fn_args, expected_result)
+
+
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            ["--mime-type", "text/plain"],
+            type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [2, 4]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "text/plain"],
+            percent_type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [33.33, 40]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "text/plain"],
+            type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 3], "B": [0, 1], "C": [2, 0]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "text/plain"],
+            percent_type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 75], "B": [0, 33.33], "C": [66.67, 0]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "image/heic"],
+            type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [0, 1]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "image/heic"],
+            percent_type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [0, 10]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "image/heic"],
+            type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 1], "B": [0, 0], "C": [0, 0]},
+            },
+        ),
+        (
+            "group",
+            ["--mime-type", "image/heic"],
+            percent_type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 25], "B": [0, 0], "C": [0, 0]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "text/plain"],
+            type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [2, 5]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "text/plain"],
+            percent_type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [33.33, 45.45]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "text/plain"],
+            type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 3], "B": [2, 2]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "text/plain"],
+            percent_type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 50], "B": [66.67, 40]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "image/heic"],
+            type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [0, 1]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "image/heic"],
+            percent_type_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [0, 9.09]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "image/heic"],
+            type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 1], "B": [0, 0]},
+            },
+        ),
+        (
+            "non_group",
+            ["--mime-type", "image/heic"],
+            percent_type_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [0, 16.67], "B": [0, 0]},
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        MimeType(),
+        "mime_type",
+        csv,
+        fn_args,
+        category,
+        graph_individual,
+        expected_result,
+    )
