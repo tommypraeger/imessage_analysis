143fd1bd78857662db7961c9945a986b3ba5d0d8
Tommy Praeger <tommypraeger@gmail.com>
2023-02-16 22:12:23 -0800

    parametrize tests - total

--- analysis/tests/functions/total_test.py
+++ analysis/tests/functions/total_test.py
@@ -1,3 +1,4 @@
+import pytest
 from analysis.functions.total import (
     Total,
     total_messages_category,
@@ -6,106 +7,101 @@ from analysis.functions.total import (
 from analysis.tests.testutils import *
 
 
-total_test = FunctionTest(Total(), "total")
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            [],
+            {
+                "names": ["A", "B", "C"],
+                total_messages_category: [6, 3, 3],
+                percent_total_messages_category: [50, 25, 25],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            {
+                "names": ["A", "B"],
+                total_messages_category: [5, 5],
+                percent_total_messages_category: [50, 50],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(Total(), "total", csv, fn_args, expected_result)
 
 
-def test_table__group():
-    total_test.run_table_test(
-        csv="group",
-        fn_args=[],
-        expected_result={
-            "names": ["A", "B", "C"],
-            total_messages_category: [6, 3, 3],
-            percent_total_messages_category: [50, 25, 25],
-        },
-    )
-
-
-def test_table__non_group():
-    total_test.run_table_test(
-        csv="non_group",
-        fn_args=[],
-        expected_result={
-            "names": ["A", "B"],
-            total_messages_category: [5, 5],
-            percent_total_messages_category: [50, 50],
-        },
-    )
-
-
-def test_graph__group__total__total_messages():
-    total_test.run_graph_test(
-        csv="group",
-        fn_args=[],
-        category=total_messages_category,
-        graph_individual=False,
-        expected_result={
-            "datasets": {"Total": [4, 8]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    )
-
-
-def test_graph__group__individual__total_messages():
-    total_test.run_graph_test(
-        csv="group",
-        fn_args=[],
-        category=total_messages_category,
-        graph_individual=True,
-        expected_result={
-            "datasets": {"A": [2, 4], "B": [1, 2], "C": [1, 2]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    )
-
-
-def test_graph__group__individual__percent_messages():
-    total_test.run_graph_test(
-        csv="group",
-        fn_args=[],
-        category=percent_total_messages_category,
-        graph_individual=True,
-        expected_result={
-            "datasets": {"A": [50, 50], "B": [25, 25], "C": [25, 25]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    )
-
-
-def test_graph__non_group__total__total_messages():
-    total_test.run_graph_test(
-        csv="non_group",
-        fn_args=[],
-        category=total_messages_category,
-        graph_individual=False,
-        expected_result={
-            "datasets": {"Total": [5, 5]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    )
-
-
-def test_graph__non_group__individual__total_messages():
-    total_test.run_graph_test(
-        csv="non_group",
-        fn_args=[],
-        category=total_messages_category,
-        graph_individual=True,
-        expected_result={
-            "datasets": {"A": [3, 2], "B": [2, 3]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    )
-
-
-def test_graph__non_group__individual__percent_messages():
-    total_test.run_graph_test(
-        csv="non_group",
-        fn_args=[],
-        category=percent_total_messages_category,
-        graph_individual=True,
-        expected_result={
-            "datasets": {"A": [60, 40], "B": [40, 60]},
-            "labels": ["1/1/00", "1/2/00"],
-        },
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            [],
+            total_messages_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [4, 8]},
+            },
+        ),
+        (
+            "group",
+            [],
+            total_messages_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [2, 4], "B": [1, 2], "C": [1, 2]},
+            },
+        ),
+        (
+            "group",
+            [],
+            percent_total_messages_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [50, 50], "B": [25, 25], "C": [25, 25]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            total_messages_category,
+            False,
+            {
+                "datasets": {"Total": [5, 5]},
+                "labels": ["1/1/00", "1/2/00"],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            total_messages_category,
+            True,
+            {
+                "datasets": {"A": [3, 2], "B": [2, 3]},
+                "labels": ["1/1/00", "1/2/00"],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            percent_total_messages_category,
+            True,
+            {
+                "datasets": {"A": [60, 40], "B": [40, 60]},
+                "labels": ["1/1/00", "1/2/00"],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        Total(), "total", csv, fn_args, category, graph_individual, expected_result
     )
--- analysis/tests/testutils.py
+++ analysis/tests/testutils.py
@@ -1,99 +1,59 @@
 from datetime import datetime
 import pandas as pd
 import analysis.utils.constants as constants
-import pytest
 from analysis.utils.helpers import is_reaction
 from analysis.utils.parse_args import get_analysis_args
 
 
-class FunctionTest:
-    def __init__(self, fn_class, fn_name):
-        self.fn_class = fn_class
-        self.fn_name = fn_name
-
-    def run_table_test(self, csv, fn_args, expected_result):
-        df = load_csv(self.fn_name, csv)
-        test_args = (
-            df,
-            parse_fn_args(self.fn_name, "table", *fn_args),
-            get_chat_members(df),
-        )
-        result = self.fn_class.run(*test_args)
-        try:
-            assert result == expected_result
-        except AssertionError:
-            print(f"actual: {result}")
-            print(f"expected: {expected_result}")
-            raise AssertionError
-
-    def run_graph_test(self, csv, fn_args, category, graph_individual, expected_result):
-        df = load_csv(self.fn_name, csv)
-        if graph_individual:
-            fn_args.append("--graph-individual")
-        test_args = (
-            df,
-            parse_fn_args(
-                self.fn_name,
-                "graph",
-                "--graph-time-interval",
-                "day",
-                "--category",
-                category,
-                *fn_args,
-            ),
-            get_chat_members(df),
-        )
-        result = self.fn_class.run(*test_args)
-        try:
-            assert result["labels"] == expected_result["labels"]
-            for label, data in expected_result["datasets"].items():
-                found = False
-                for dataset in result["datasets"]:
-                    if dataset["label"] == label and dataset["data"] == data:
-                        found = True
-                assert found
-        except AssertionError:
-            print(f"actual: {result}")
-            print(f"expected: {expected_result}")
-            raise AssertionError
+def run_table_test(fn_class, fn_name, csv, fn_args, expected_result):
+    df = load_csv(fn_name, csv)
+    test_args = (
+        df,
+        parse_fn_args(fn_name, "table", *fn_args),
+        get_chat_members(df),
+    )
+    result = fn_class.run(*test_args)
+    assert_equal(result, expected_result)
 
 
-def assert_equal(actual, expected, description, test_args):
+def run_graph_test(
+    fn_class, fn_name, csv, fn_args, category, graph_individual, expected_result
+):
+    df = load_csv(fn_name, csv)
+    if graph_individual:
+        fn_args.append("--graph-individual")
+    test_args = (
+        df,
+        parse_fn_args(
+            fn_name,
+            "graph",
+            "--graph-time-interval",
+            "day",
+            "--category",
+            category,
+            *fn_args,
+        ),
+        get_chat_members(df),
+    )
+    result = fn_class.run(*test_args)
+    transform_graph_result(result)
+    assert_equal(result, expected_result)
+
+
+def transform_graph_result(graph_result):
+    dataset_dict = {}
+    for dataset in graph_result["datasets"]:
+        dataset_dict[dataset["label"]] = dataset["data"]
+    graph_result["datasets"] = dataset_dict
+
+
+def assert_equal(actual, expected):
     try:
         assert actual == expected
     except AssertionError:
-        print(
-            f"Failed.\n"
-            f"Description: {description}\n"
-            f"Test args: {test_args}\n"
-            f"Actual: {actual}\n"
-            f"Expected: {expected}"
-        )
-        raise (AssertionError)
-
-
-def assert_table_results_correct(actual, expected):
-    for i in range(len(actual)):
-        # remove description keys
-        description = expected[i]["description"]
-        del expected[i]["description"]
-        test_args = actual[i]["test args"]
-        del actual[i]["test args"]
-        assert_equal(actual[i], expected[i], description, test_args)
-
-
-def assert_graph_results_correct(actual, expected):
-    for i in range(len(actual)):
-        assert actual[i]["labels"] == expected[i]["labels"]
-        for dataset in range(len(actual[i]["datasets"])):
-            description = expected[i]["description"]
-            test_args = actual[i]["test args"]
-            actual_label = actual[i]["datasets"][dataset]["label"]
-            expected_label = expected[i]["datasets"][dataset]["label"]
-            assert_equal(actual_label, expected_label, description, test_args)
-            actual_data = actual[i]["datasets"][dataset]["data"]
-            expected_data = expected[i]["datasets"][dataset]["data"]
-            assert_equal(actual_data, expected_data, description, test_args)
+        print(f"Actual:   {actual}")
+        print(f"Expected: {expected}")
+        raise AssertionError
 
 
 def get_chat_members(df):
@@ -125,100 +85,15 @@ def parse_fn_args(function, output_type, *fn_args):
     )
 
 
-def build_printable_test_args(
-    fn_name, csv, fn_args, output_type, category=None, indvidual=False
-):
-    ret = {"function": fn_name, "csv": csv, "fn args": fn_args, "output": output_type}
-    if output_type == "graph":
-        ret["category"] = category
-        ret["graph individual"] = indvidual
-    return ret
-
-
-def generate_table_test_result(
-    fn_class,
-    fn_name,
-    csvs,
-    fn_args_combos,
-):
-    results = []
-    for csv in csvs:
-        df = load_csv(fn_name, csv)
-        for fn_args in fn_args_combos:
-            test_args = (
-                df,
-                parse_fn_args(fn_name, "table", *fn_args),
-                get_chat_members(df),
-            )
-            try:
-                result = fn_class.run(*test_args)
-            except Exception as e:
-                print(e)
-                result = {}
-            result["test args"] = build_printable_test_args(
-                fn_name, csv, fn_args, "table"
-            )
-            results.append(result)
-    return results
-
-
-def generate_graph_test_result(
-    fn_class,
-    fn_name,
-    csvs,
-    graph_total_categories,
-    graph_individual_categories,
-    fn_args_combos,
-):
-    results = []
-    for csv in csvs:
-        df = load_csv(fn_name, csv)
-        for fn_args in fn_args_combos:
-            for category in graph_total_categories:
-                test_args = (
-                    df,
-                    parse_fn_args(
-                        fn_name,
-                        "graph",
-                        "--graph-time-interval",
-                        "day",
-                        "--category",
-                        category,
-                        *fn_args,
-                    ),
-                    get_chat_members(df),
-                )
-                try:
-                    result = fn_class.run(*test_args)
-                except Exception as e:
-                    print(e)
-                    result = {}
-                result["test args"] = build_printable_test_args(
-                    fn_name, csv, fn_args, "graph", category, indvidual=False
-                )
-                results.append(result)
-            for category in graph_individual_categories:
-                test_args = (
-                    df,
-                    parse_fn_args(
-                        fn_name,
-                        "graph",
-                        "--graph-individual",
-                        "--graph-time-interval",
-                        "day",
-                        "--category",
-                        category,
-                        *fn_args,
-                    ),
-                    get_chat_members(df),
-                )
-                try:
-                    result = fn_class.run(*test_args)
-                except Exception as e:
-                    print(e)
-                    result = {}
-                result["test args"] = build_printable_test_args(
-                    fn_name, csv, fn_args, "graph", category, indvidual=True
-                )
-                results.append(result)
-    return results
+def format_param(param):
+    if isinstance(param, str):
+        return param
+    if isinstance(param, list):
+        return str(param)
+    if isinstance(param, bool):
+        if param:
+            return "individual"
+        return "total"
+    if isinstance(param, dict):
+        # do not attempt to format expected results
+        return ""
