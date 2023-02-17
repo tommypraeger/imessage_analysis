833eecc8f9d3dce0a1f597682eb27b1b5658c8ea
Tommy Praeger <tommypraeger@gmail.com>
2023-02-16 23:50:41 -0800

    parametrize tests - word count, word length

--- analysis/tests/functions/word_count_test.py
+++ analysis/tests/functions/word_count_test.py
@@ -1,91 +1,87 @@
+import pytest
 from analysis.functions.word_count import WordCount, average_word_count_category
 from analysis.tests.testutils import *
 
 
-def test_word_count():
-    fn = WordCount()
-    table_actual = [
-        result
-        for result in generate_table_test_result(
-            fn, "word_count", csvs=["group", "non_group"], fn_args_combos=[[]]
-        )
-    ]
-    graph_actual = [
-        result
-        for result in generate_graph_test_result(
-            fn,
-            "word_count",
-            csvs=["group", "non_group"],
-            graph_total_categories=fn.get_categories_allowing_graph_total(),
-            graph_individual_categories=fn.get_categories(),
-            fn_args_combos=[[]],
-        )
-    ]
-    table_expected = [
-        {
-            "description": "group",
-            "names": ["A", "B", "C"],
-            average_word_count_category: [1, 2, 3],
-        },
-        {
-            "description": "non-group",
-            "names": ["A", "B"],
-            average_word_count_category: [1, 2],
-        },
-    ]
-    graph_expected = [
-        {
-            "description": "group, total, average",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1.8, 1.75],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, average",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [1, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [2.33, 1.5],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 3],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, average",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [1.8, 1.25],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, average",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [1, 1],
-                },
-                {
-                    "label": "B",
-                    "data": [2.33, 1.5],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    ]
-    assert_table_results_correct(table_actual, table_expected)
-    assert_graph_results_correct(graph_actual, graph_expected)
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            [],
+            {
+                "names": ["A", "B", "C"],
+                average_word_count_category: [1, 2, 3],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            {
+                "names": ["A", "B"],
+                average_word_count_category: [1, 2],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(WordCount(), "word_count", csv, fn_args, expected_result)
+
+
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            [],
+            average_word_count_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1.8, 1.75]},
+            },
+        ),
+        (
+            "group",
+            [],
+            average_word_count_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [1, 1], "B": [2.33, 1.5], "C": [0, 3]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            average_word_count_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [1.8, 1.25]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            average_word_count_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [1, 1], "B": [2.33, 1.5]},
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        WordCount(),
+        "word_count",
+        csv,
+        fn_args,
+        category,
+        graph_individual,
+        expected_result,
+    )
--- analysis/tests/functions/word_length_test.py
+++ analysis/tests/functions/word_length_test.py
@@ -1,91 +1,87 @@
+import pytest
 from analysis.functions.word_length import WordLength, average_word_length_category
 from analysis.tests.testutils import *
 
 
-def test_word_length():
-    fn = WordLength()
-    table_actual = [
-        result
-        for result in generate_table_test_result(
-            fn, "word_length", csvs=["group", "non_group"], fn_args_combos=[[]]
-        )
-    ]
-    graph_actual = [
-        result
-        for result in generate_graph_test_result(
-            fn,
-            "word_length",
-            csvs=["group", "non_group"],
-            graph_total_categories=fn.get_categories_allowing_graph_total(),
-            graph_individual_categories=fn.get_categories(),
-            fn_args_combos=[[]],
-        )
-    ]
-    table_expected = [
-        {
-            "description": "group",
-            "names": ["A", "B", "C"],
-            average_word_length_category: [3, 2, 2],
-        },
-        {
-            "description": "non-group",
-            "names": ["A", "B"],
-            average_word_length_category: [3, 2],
-        },
-    ]
-    graph_expected = [
-        {
-            "description": "group, total, average",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [2.11, 2.25],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "group, individual, average",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [2.5, 4],
-                },
-                {
-                    "label": "B",
-                    "data": [2, 2],
-                },
-                {
-                    "label": "C",
-                    "data": [0, 2],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, total, average",
-            "datasets": [
-                {
-                    "label": "Total",
-                    "data": [2.11, 2.5],
-                }
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-        {
-            "description": "non-group, individual, average",
-            "datasets": [
-                {
-                    "label": "A",
-                    "data": [2.5, 3.5],
-                },
-                {
-                    "label": "B",
-                    "data": [2, 2],
-                },
-            ],
-            "labels": ["1/1/00", "1/2/00"],
-        },
-    ]
-    assert_table_results_correct(table_actual, table_expected)
-    assert_graph_results_correct(graph_actual, graph_expected)
+@pytest.mark.parametrize(
+    "csv,fn_args,expected_result",
+    [
+        (
+            "group",
+            [],
+            {
+                "names": ["A", "B", "C"],
+                average_word_length_category: [3, 2, 2],
+            },
+        ),
+        (
+            "non_group",
+            [],
+            {
+                "names": ["A", "B"],
+                average_word_length_category: [3, 2],
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_table(csv, fn_args, expected_result):
+    run_table_test(WordLength(), "word_length", csv, fn_args, expected_result)
+
+
+@pytest.mark.parametrize(
+    "csv,fn_args,category,graph_individual,expected_result",
+    [
+        (
+            "group",
+            [],
+            average_word_length_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [2.11, 2.25]},
+            },
+        ),
+        (
+            "group",
+            [],
+            average_word_length_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [2.5, 4], "B": [2, 2], "C": [0, 2]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            average_word_length_category,
+            False,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"Total": [2.11, 2.5]},
+            },
+        ),
+        (
+            "non_group",
+            [],
+            average_word_length_category,
+            True,
+            {
+                "labels": ["1/1/00", "1/2/00"],
+                "datasets": {"A": [2.5, 3.5], "B": [2, 2]},
+            },
+        ),
+    ],
+    ids=format_param,
+)
+def test_graph(csv, fn_args, category, graph_individual, expected_result):
+    run_graph_test(
+        WordLength(),
+        "word_length",
+        csv,
+        fn_args,
+        category,
+        graph_individual,
+        expected_result,
+    )
