d4f6bbd171413a1cc37dabe19837e1dbaf01d2a0
Tommy Praeger <tommypraeger@gmail.com>
2025-08-29 09:09:41 -0400

    install tailwindcss

--- ui/vite.config.mjs
+++ ui/vite.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       state: fileURLToPath(new URL('./src/state', import.meta.url)),
+      components: fileURLToPath(new URL('./src/components', import.meta.url)),
     },
   },
   server: {
