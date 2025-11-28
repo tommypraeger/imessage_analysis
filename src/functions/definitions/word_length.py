8b5c48bba26084e7c24633ce341d3bb4d143eb23
Tommy Praeger <tommypraeger@gmail.com>
2025-11-27 21:55:34 -0500

    more word filtering for word length

--- src/functions/definitions/word_length.py
+++ src/functions/definitions/word_length.py
@@ -1,6 +1,8 @@
 import math
+import re
 
 from src.functions import Function
+from src.functions.definitions.word_count import add_word_count_columns
 from src.utils import helpers, constants
 
 average_word_length_category = "Average letters per word"
@@ -21,12 +23,8 @@ class WordLength(Function):
 
     @staticmethod
     def process_messages_df(df, args):
+        df = add_word_count_columns(df)
         text = df["text"].astype("string")
-        mt = df["message_type"].astype("string")
-        not_reaction = ~mt.isin(constants.REACTION_TYPES)
-        link_mask = text.str.match(constants.LINK_REGEX, na=False)
-        df["is link?"] = (not_reaction & link_mask)
-        df["word count"] = text.str.split().str.len().fillna(0).astype("int64")
         # Letters only: strip non-letters then length
         letters = text.str.replace("[^a-zA-Z]+", "", regex=True)
         df["letter count"] = letters.str.len().fillna(0).astype("int64")
@@ -35,12 +33,28 @@ class WordLength(Function):
     @staticmethod
     def get_results(output_dict, df, args, member_name=None, time_period=None):
         nr_messages = helpers.get_non_reaction_messages(df, member_name, time_period)
-        total_words = nr_messages[
-            (nr_messages["word count"] != 0) & (~nr_messages["is link?"])
-        ]["word count"].sum()
-        total_letters = nr_messages[
-            (nr_messages["word count"] != 0) & (~nr_messages["is link?"])
-        ]["letter count"].sum()
+        words_series = nr_messages["text"].astype("string").str.split()
+
+        def is_valid_word(word: str) -> bool:
+            if len(word) > 20:
+                return False
+            if re.search(r"(.)\\1{4}", word):
+                return False
+            return True
+
+        def clean_letters(word: str) -> str:
+            return re.sub(r"[^a-zA-Z]+", "", word)
+
+        filtered_words = words_series.apply(
+            lambda words: [w for w in words if is_valid_word(w)] if isinstance(words, list) else []
+        )
+        filtered_counts = filtered_words.apply(len)
+        filtered_letters = filtered_words.apply(lambda ws: sum(len(clean_letters(w)) for w in ws if clean_letters(w)))
+
+        mask = (~nr_messages["is link?"]) & (filtered_counts > 0)
+
+        total_words = filtered_counts[mask].sum()
+        total_letters = filtered_letters[mask].sum()
         average_word_length = round(helpers.safe_divide(total_letters, total_words), 2)
         if math.isnan(average_word_length):
             average_word_length = 0
